f91a48dd82bd6eb31d6b1e2b7b11d2cde372f93e
Izmaylova <Natalyatf@yandex.ru>
2023-04-16 19:28:31 +0500

    initial commit

---
+++ src/test/java/com/example/animalshelderbot/AnimalShelderBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.animalshelderbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnimalShelderBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
